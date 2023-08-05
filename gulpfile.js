743625b734a5a7b084c39c9fbf5821d518560abd
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-08-05 08:45:39 +0200

    updated gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-//const purgecss = require('gulp-purgecss');
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return (
-    src('sass/**/*.scss')
-      .pipe(sass())
-      //.pipe(purgecss({ content: ['*.html'] }))
-      .pipe(dest('css'))
-  );
+  return src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['uie/**/*.scss'], buildStyles);
-  //watch(['uie/**/*.scss', '*.html'], buildStyles);
+  watch(['uie/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
